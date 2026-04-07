565c32df2b207816844164e7a7b4cd4c7914453d
Diego <diegoalejandrorc22@gmail.com>
2026-04-07 11:58:27 +0200

    creación de la clase Nota, GestorNotas y arranque básico para el main

---
+++ src/GestorNotas.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorNotas {
+
+    private List<Nota> notas;
+
+    public GestorNotas() {
+        this.notas = new ArrayList<>();
+    }
+}
